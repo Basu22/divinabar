d65eaa9f998d0999ed46fec7bd831d94f93e6cc9
Baso <basilio.ossvald@gmail.com>
2023-11-12 00:18:56 -0300

    linea azul, nueva imagen

--- src/js/App.js
+++ src/js/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Header } from './Header/Header';
 import { Contenido } from './Contenido/Contenido';
-import { Footer } from './Footer/Footer';
+/* import { Footer } from './Footer/Footer'; */
 import { PanelAdmin } from './Admin/panelAdmin';
 
 
